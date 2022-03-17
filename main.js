1a7a006cca4fc167eb5b4a5853986c8dec8fc681
Andressa Varela <andressaavarela@gmail.com>
2022-03-17 15:46:42 -0300

    layout changes

--- main.js
+++ main.js
@@ -16,3 +16,17 @@ for (const link of links) {
     nav.classList.remove('show')
   })
 }
+
+/* mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+}
