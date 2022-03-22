4683bcd21100db9bd9c08bc56e86fccbcddf94d2
Andressa Varela <andressaavarela@gmail.com>
2022-03-21 22:12:09 -0300

    final changes

--- main.js
+++ main.js
@@ -20,8 +20,7 @@ for (const link of links) {
 /* mudar o header da página quando der scroll */
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     //scroll é maior q altura do header
     header.classList.add('scroll')
@@ -29,4 +28,61 @@ window.addEventListener('scroll', function () {
     // scroll é menor q altura do header
     header.classList.remove('scroll')
   }
+}
+
+// Scroll reveal
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#about .image, #about .text, 
+  #location .image, #location .text,
+  #contact .text, #contact .links,
+  footer .social, footer .brand
+  `,
+  { interval: 100 }
+)
+
+/* botão voltar para o topo */
+const backToTopButton = document.querySelector('.back-to-top')
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
 })
